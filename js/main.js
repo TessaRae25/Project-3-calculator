1e73ef4d369011f0c807611afaeee30ddf6de6fa
Tess Lim <tess@Tesss-MacBook-Pro.local>
2017-04-24 21:35:14 -0400

    project-3-calculator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+var calc = document.querySelector('#display');
 
+function displayScreen(x) {
+	calc.value += x;
+
+	if(x == 'c') {
+		calc.value = '';
+	}
+}
+
+function solution() {
+	x = calc.value;
+	x = eval(x);
+	calc.value = x;
+}
+
+function backspace() {
+	var value = calc.value;
+	var length = value.length -1;
+	var newVariable = value.substring(0,length);
+	calc.value = newVariable;
+}
